886d7498c91f209ac349ccd40a7ebc8ed21146dd
Nolaaaaa <1009198372@qq.com>
2018-12-18 14:31:10 +0800

    减少html中的死数据

--- js/project.js
+++ js/project.js
@@ -1,12 +1,57 @@
-!function() {
-    fetch('./json/project.json')   
-        .then(response => response.json()) 
-        .then(json => json.data.projects)
-        .then(renderProject)   
+!function () {
+  fetch('./json/project.json')
+    .then(response => response.json())
+    .then(json => json.data.projects)
+    .then(renderProject)
 
-    function renderProject(lists) {
-        document.querySelector('.section_body').innerHTML = lists.map(list => 
-            `<section class="project">
+  fetch('./json/skill.json')
+    .then(response => response.json())
+    .then(json => json.data.skills)
+    .then(renderSkills)
+
+
+  fetch('./json/companyAndHobby.json')
+    .then(response => response.json())
+    .then(json => json.data.companys)
+    .then(renderCompanys)
+
+  fetch('./json/companyAndHobby.json')
+    .then(response => response.json())
+    .then(json => json.data.hobbys)
+    .then(renderHobbys)
+
+
+  // 工作经历
+  function renderCompanys(lists) {
+    document.querySelector('.companys').innerHTML = lists.map(list =>
+      `<section class="company">
+          <h3>${list.name}</h3>
+          <ul>${contentLists(list.contents)}</ul>
+        </section>`
+    ).join('')
+    function contentLists(lists) {
+      return lists.map(list =>
+        `<li>${list.content}</li>`
+      ).join('')
+    }
+  }
+
+  // 自我描述
+  function renderHobbys(lists) {
+    document.querySelector('.hobbys').innerHTML = lists.map(list =>
+      `<ul>${contentLists(list.contents)}</ul>`
+    ).join('')
+    function contentLists(lists) {
+      return lists.map(list =>
+        `<li>${list.content}</li>`
+      ).join('')
+    }
+  }
+
+  // 项目及描述
+  function renderProject(lists) {
+    document.querySelector('.sectionBody').innerHTML = lists.map(list =>
+      `<section class="project">
             <div class="project_intro">
                 <header class="project_intro_header clearfix">
                     <h4 class="project_intro_header_title">${list.title}<span>（${list.Etitle}）</span></h4>
@@ -25,27 +70,42 @@
                 <img src="${list.image}" alt="${list.imageAlt}" width="356px" height="267px">
             </div>
         </section>`
-        ).join('')    
-        function skillLists(lists) {
-            return lists.map(list => 
-                `<li class="tech-tag-list_item ">${list.skill}</li>`
-            ).join('') 
-        }
-        function functionLists(lists) {
-            return lists.map(list => 
-                `
-                <li>${list.function}</li>
-                `
-            ).join('') 
-        }
+    ).join('')
+    function skillLists(lists) {
+      return lists.map(list =>
+        `<li class="tech-tag-list_item ">${list.skill}</li>`
+      ).join('')
     }
-    window.onload = function itemlists() {   //window.onload，即，在页面所有的一切都加载完后才执行函数
-        let item = document.querySelectorAll('.tech-tag-list_item')
-        for(let i=0; i < item.length; i++) {
-            let value =  item[i].innerText
-            if(value){
-                item[i].classList.add(`tech-tag-list_${value}`)
-            }
-        }
+    function functionLists(lists) {
+      return lists.map(list =>
+        `<li>${list.function}</li>`
+      ).join('')
     }
+  }
+
+  // 技能描述
+  function renderSkills(lists) {
+    document.querySelector('.printSkill').innerHTML = lists.map(list =>
+      `<section class="skills">
+            <h3>${list.title}</h3>
+            <ul>${skillLists(list.skills)}</ul>
+          </section>`
+    ).join('')
+    function skillLists(lists) {
+      return lists.map(list =>
+        `<li>${list.skill}</li>`
+      ).join('')
+    }
+  }
+
+  // 不同的技能不同的颜色
+  window.onload = function itemlists() {   //window.onload，即，在页面所有的一切都加载完后才执行函数
+    let item = document.querySelectorAll('.tech-tag-list_item')
+    for (let i = 0; i < item.length; i++) {
+      let value = item[i].innerText
+      if (value) {
+        item[i].classList.add(`tech-tag-list_${value}`)
+      }
+    }
+  }
 }.call()
